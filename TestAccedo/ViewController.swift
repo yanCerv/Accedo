17a5f98a4b7ca5f1010ca9ab2367eacbc2a5d8fb
Adrian Cervantes <zeus481@hotmail.com>
2019-10-29 17:12:16 -0600

    Initial Commit

---
+++ TestAccedo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestAccedo
+//
+//  Created by MacBook Pro on 29/10/19.
+//  Copyright © 2019 MacBook Pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TestAccedoTests/TestAccedoTests.swift
@@ -0,0 +1,34 @@
+//
+//  TestAccedoTests.swift
+//  TestAccedoTests
+//
+//  Created by MacBook Pro on 29/10/19.
+//  Copyright © 2019 MacBook Pro. All rights reserved.
+//
+
+import XCTest
+@testable import TestAccedo
+
+class TestAccedoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
